b1e367d61151bac57726d0fa94cebc45f61b454f
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2014-03-11 23:29:43 +0100

    Added message class approval process

---
+++ static/js/msg_type_for_approval.js
@@ -0,0 +1,27 @@
+/**
+ * Created with PyCharm.
+ * User: alivinco
+ * Date: 11/03/14
+ * Time: 13:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+function approve_msg_class(address,msg_class,is_approved)
+{
+   // add class to address msg_class mapping if that was approved otherwise move to block list
+   obj = {"address":address,"msg_class":msg_class,"is_approved":is_approved}
+   $.ajax({
+      url: "../api/approve_msg_class",
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      data:  JSON.stringify(obj),
+      success: function( data ) {
+//        reload page
+      }
+    });
+}
+
+(function() {
+//    console.log( "ready!" );
+//    $("#inter_console_table").stupidtable();
+});
