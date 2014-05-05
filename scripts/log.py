71a2ff20e0be1c4542b38201579447f8c9f755cb
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2014-05-05 23:05:16 +0200

    Added initi.d script and extended setup.sh script

---
+++ scripts/log.py
@@ -0,0 +1,69 @@
+__author__ = 'alivinco'
+
+config = {
+    'version': 1,
+    'disable_existing_loggers': True,  # this fixes the problem
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': "standard"
+        },
+        'info_file_handler': {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "standard",
+            "filename": "/var/log/blackfly/info.log",
+            "maxBytes": "10485760",
+            "backupCount": "20",
+            "encoding": "utf8"
+        },
+
+        'error_file_handler': {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "ERROR",
+            "formatter": "standard",
+            "filename": "/var/log/blackfly/errors.log",
+            "maxBytes": "10485760",
+            "backupCount": "20",
+            "encoding": "utf8"
+        }
+    },
+    'loggers': {
+        'werkzeug': {
+            'handlers': ['default'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+        'bf_web':{
+            "handlers": ["default", "info_file_handler", "error_file_handler"],
+            'level':'INFO',
+            'propagate': True
+        },
+        'bf_msg_pipeline':{
+            "handlers": ["default", "info_file_handler", "error_file_handler"],
+            'level':'DEBUG',
+            'propagate': True
+        },
+        'bf_mqtt':{
+            "handlers": ["default", "info_file_handler", "error_file_handler"],
+            'level':'INFO',
+            'propagate': True
+        },
+        'bf_cache':{
+            "handlers": ["default", "info_file_handler", "error_file_handler"],
+            'level':'INFO',
+            'propagate': True
+        },
+        'bf_msg_manager':{
+            "handlers": ["default", "info_file_handler", "error_file_handler"],
+            'level':'INFO',
+            'propagate': True
+        }
+    }
+}
--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+BLACK_DIR = $(pwd)
 mount - / -oremount,rw
 curl -o /tmp/get-pip.py https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 cd /tmp
 python get-pip.py
 pip install Flask
+cd $BLACK_DIR
+chmod a+x scripts/etc/init.d/blackfly
+cp -i scripts/etc/init.d/blackfly /etc/init.d/
+cp -i sripts/log.py configs/
+update-rc.d blackfly defaults
