346eb1f8f2d27eccbbd7e586debdf27e009f9704
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2014-05-07 19:02:10 +0200

    Extended setup , msg type to UI element rendering was exposed to it's own template . Added timestamp to inteconsole ,so far rendered only on server side

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-BLACK_DIR=$(pwd)
 mount - / -oremount,rw
-#curl -o /tmp/get-pip.py https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-#cd /tmp
-#python get-pip.py
 python scripts/get-pip.py
 pip install Flask
-#cd $BLACK_DIR
 chmod a+x scripts/etc/init.d/blackfly
 chmod a+x BlackflyTestSuite.py
 cp -i scripts/etc/init.d/blackfly /etc/init.d/
 cp -i scripts/log.py configs/
+# address mapping replaced by fresh address mapping
+#cp -i scripts/config/address_mapping.json configs/
 update-rc.d blackfly defaults
