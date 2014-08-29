ec6669900ab93c962321a22971f0ad699c523ec2
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2014-08-29 22:11:12 +0200

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,10 @@ cd ../
 rm -r $build_root
 mkdir $build_root
 cp -r * $build_root
-cd ../bf_build
-pwd=$(pwd)
+#cd ../bf_build
 cd /tmp
+pwd=$(pwd)
 tar -zcvf $pwd/blackfly.tar.gz BlackflyTestSuite/
+# tar -zcvf --exclude=.git --exclude=.gitignore --exclude=.idea /tmp/blackfly.tar.gz BlackflyTestSuite/
 cp $pwd/blackfly.tar.gz /var/www/blackfly/
 cp $build_root/scripts/install.sh /var/www/blackfly
