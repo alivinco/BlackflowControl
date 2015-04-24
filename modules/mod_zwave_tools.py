78d61c01be9e7ad3620358519faa4fc29f774161
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2015-04-24 16:29:30 +0200

    bugfix for mesh graph diagramm.

--- modules/mod_zwave_tools.py
+++ modules/mod_zwave_tools.py
@@ -11,17 +11,17 @@ class ZwaveTools:
         edges = []
         graph = []
 
+        # loop over nodes
         for node , nb_list in nb_table.iteritems():
             nodes.append({"data":{"id":node}})
 
-            # generating edges
-
+            # loop over neibhours and generating edges
             for nb_node in nb_list :
                 # filtering out back duplicates
-                nb_check = filter(lambda nd :(nd["data"]["id"]==str(nb_node)),nodes)
+                nb_check = filter(lambda nd :(nd["data"]["id"]=="%s_%s"%(nb_node,node)),edges)
                 if len(nb_check)==0:
-                    edge = {"data":{"id":node+"_"+str(nb_node),"weight":1,"source":node,"target":str(nb_node)}}
-                edges.append(edge)
+                    edge = {"data":{"id":"%s_%s"%(node,nb_node),"weight":1,"source":node,"target":str(nb_node)}}
+                    edges.append(edge)
 
         graph = {"nodes":nodes,"edges":edges}
 
