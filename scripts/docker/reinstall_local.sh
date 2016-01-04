f32e07c8d36db1388bfaaebdebb25a46751e2a0c
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2016-01-04 10:50:56 +0200

    docker reinstall script improvements

--- scripts/docker/reinstall_local.sh
+++ scripts/docker/reinstall_local.sh
@@ -2,6 +2,6 @@
 pname = alivinco/blackfly
 docker stop $pname
 docker rm $pname
-docker run --name blackfly -p 5001:5000 -d -e VIRTUAL_HOST=blackfly $pname
+sudo docker run --name nginx-master -d -t -p 80:80 -p 443:443 -v /etc/ssl/private/:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock:ro alivinco/nginx-proxy
 
 
