ba20102e0f4d75ddbdb942ac878bc59b8d04e10a
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2016-01-08 18:55:24 +0200

    Now sidebar  saves it's state between page reload . State is stored in browser local store

--- static/angulr/html/js/ui-toggle.js
+++ static/angulr/html/js/ui-toggle.js
@@ -1,7 +1,18 @@
 +function ($) {
 
   $(function(){
-
+      var toggles = localStorage.getItem("toggles")
+		if (!toggles) {
+		  toggles = {".app":true}
+		} else {
+		  toggles = JSON.parse(toggles)
+		}
+	  if (!toggles[".app"]) {
+		 $(".app").addClass("app-aside-folded")
+	  } else {
+		 $(".app").removeClass("app-aside-folded")
+	  }
+	  //console.dir($(".app"))
       $(document).on('click', '[ui-toggle-class]', function (e) {
         e.preventDefault();
         var $this = $(e.target);
@@ -10,11 +21,19 @@
 		var classes = $this.attr('ui-toggle-class').split(','),
 			targets = ($this.attr('target') && $this.attr('target').split(',')) || Array($this),
 			key = 0;
+
 		$.each(classes, function( index, value ) {
+
 			var target = targets[(targets.length && key)];
 			$( target ).toggleClass(classes[index]);
+			//localStorage.setItem(target,classes[index])
+			if (toggles[target])
+				{toggles[target] = false}
+			else
+				{toggles[target] = true}
 			key ++;
 		});
+		localStorage.setItem("toggles",JSON.stringify(toggles))
 		$this.toggleClass('active');
 
       });
